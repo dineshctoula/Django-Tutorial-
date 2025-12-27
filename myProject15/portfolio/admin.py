fcc4dd0ecc44798d9db156af2a9d6ef8978e9ca3
dineshctoula <ds9806026041@gmail.com>
2025-12-27 09:33:05 +0545

    27. Register and manage models in django admin

--- myProject15/portfolio/admin.py
+++ myProject15/portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student
+from .models import Profile
 
 # Register your models here.
+# model lai register garnu parxa admin ma 
+
+admin.site.register(Student)
+admin.site.register(Profile)
--- myProject15/portfolio/models.py
+++ myProject15/portfolio/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Student( models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    city=models.CharField(max_length=100)
+
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+class Profile(models.Model):
+    bio=models.TextField()
+    location=models.CharField(max_length=100)
+    birth_date=models.DateField(null=True, blank=True)
+    # null= true bhaneko chae khali xodey pane bhayo 
+
+    
+    # blank=true , bhaneko chae form can be submitted withotut this field 
+
+
+    def __str__(self):
+        return self.location
+    
+
+    # table ma S automatically lagxah . it is by default 
