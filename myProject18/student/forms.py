9304f7a6b5a37dbd5b6b6422c16a2ce585c85b6b
dineshctoula <ds9806026041@gmail.com>
2026-01-17 09:35:14 +0545

    #31. DjangoModelForms

---
+++ myProject18/student/forms.py
@@ -0,0 +1,29 @@
+
+        # meta tells which model and fields to use 
+        
+
+       
+            # gets the value entered by the user for age 
+           
+            # yesle chae age ma validation dinxa 
+
+
+
+
+
+from django import forms
+from .models import Student
+
+
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'age', 'email']
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age < 18:
+            raise forms.ValidationError("Age must be at least 18.")
+        return age
---
+++ myProject18/student/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .forms import StudentForm
+
+# Create your views here.
+def student_create(request):
+    form=StudentForm()
+    if request.method=='POST':
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'student_success.html')
+    return render (request,'student_form.html',{'form':form})
